009a6cfdcaa3e24016cc89b74225231d2241855b
Anders Skipper <asje15@student.aau.dk>
2017-12-21 09:31:04 +0100

    New scratch start with new form and program

--- ServerSoftware/Server/Server/Program.cs
+++ ServerSoftware/Server/Server/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
-
-namespace Server
+namespace Clin
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
