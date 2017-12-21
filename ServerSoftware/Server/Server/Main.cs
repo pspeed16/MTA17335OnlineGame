4f2fe6c773e3b07f79e210718a8799cf84071f15
msk4 <mskr16@student.aau.dk>
2017-12-21 11:11:59 +0100

    Get set til sck fra Client.cs. Sat den under client_Received method i Main classen.

--- ServerSoftware/Server/Server/Main.cs
+++ ServerSoftware/Server/Server/Main.cs
@@ -16,6 +16,7 @@ namespace Server
     {
         int clientCounter = 0;
         Listener listener;
+        private static byte[] buffer = new byte[4096];
         public Main()
         {
             InitializeComponent();
@@ -72,9 +73,11 @@ namespace Server
 
                     if (client.ID == sender.ID)
                     {
+                        client.sck.Send(data);
                         int realData = BitConverter.ToInt16(data, 0);
                         lstClient.Items[i].SubItems[2].Text = realData.ToString();
                         lstClient.Items[i].SubItems[3].Text = DateTime.Now.ToString();
+                        
                         break;
                     }
                 }
